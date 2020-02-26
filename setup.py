fba64c52e5696966442cc5af00d89ae79646b7bc
adaj <adelson.dias@gmail.com>
2020-02-26 17:31:43 -0300

    cleaning the repo

--- setup.py
+++ setup.py
@@ -2,20 +2,19 @@ from setuptools import setup
 
 setup(
     name='geohunter',
-    version='0.1.0',    
+    version='0.1.2',
     description="Geographic feature extraction for machine learning with OpenStreetMap's data",
     url='https://github.com/adaj/geohunter',
-    author="Adelson Araujo jr",
-    author_email='adelsondias@gmail.com',
+    author="Adelson Araujo",
+    author_email='adelson.dias@gmail.com',
     license='MIT',
     packages=['geohunter'],
     python_requires='>=3.6',
-    install_requires=['scikit-learn', 'pandas', 'pysal', 'dill'],
-
+    install_requires=['scikit-learn', 'pandas',
+                        'geopandas', 'pysal', 'dill'],
     classifiers=[
         'Development Status :: 1 - Planning',
         'Intended Audience :: Science/Research',
         'License :: OSI Approved :: MIT License'
     ],
 )
-
